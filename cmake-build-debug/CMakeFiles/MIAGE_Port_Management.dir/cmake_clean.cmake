327f40ae463dc1b33e5c4dda0ef62fd885dc1813
Aurélien <orelienbertrand@gmail.com>
2020-03-14 21:37:10 +0100

    initial commit

---
+++ cmake-build-debug/CMakeFiles/MIAGE_Port_Management.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MIAGE_Port_Management.dir/main.cpp.o"
+  "MIAGE_Port_Management"
+  "MIAGE_Port_Management.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MIAGE_Port_Management.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
